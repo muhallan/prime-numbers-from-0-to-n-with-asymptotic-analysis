b0b8f8a92ea9609b421381fca47342dd86cad842
Muhwezi Allan <Muhwezi Allan>
2017-04-18 12:48:23 +0300

    reordered the test checks

--- prime_numbers.py
+++ prime_numbers.py
@@ -4,14 +4,14 @@ def prime_numbers(number):
     """Function to generate prime numbers from 0 to number."""
     prime_nums = []
 
+    if not isinstance(number, int):
+        return "Only integers are allowed."
+
     if number < 0:
         return "Negative numbers cannot be prime."
 
     if number in (0, 1):
         return "Zero or One cannot be prime numbers."
-
-    if not isinstance(number, int):
-        return "Only integers are allowed."
 
     for i in range(2, number + 1):
         if i > 1:
